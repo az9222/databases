30e56048ee001d65611882d5a55b9ef7ab3e23e0
az9222 <angelazhou922@gmail.com>
2018-10-10 20:00:55 -0700

    progress

--- server/db/index.js
+++ server/db/index.js
@@ -11,9 +11,9 @@ var mysql = require('mysql');
 // });
 
 var connection = mysql.createConnection({
-      user: 'student',
-      password: 'student',
-      database: 'chat'
+  user: 'student',
+  password: 'student',
+  database: 'chat'
 });
 
 
